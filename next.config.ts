d674adaaadadf723165b2b4ccd3c1cf5d7d875ce
Precious Umeha <umehaprecious@gmail.com>
2026-04-09 13:19:41 +0100

    modify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  basePath: "/my-calculator-app",
-  trailingSlash: true,
-  assetPrefix: "/my-calculator-app/",
 };
 
 export default nextConfig;
